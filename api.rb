82e3670881cb770d12df6509b4f0e0d02612a11a
Clint Laskowski <clint.laskowski@gmail.com>
2013-05-28 09:16:27 -0500

    first commit

---
+++ api.rb
@@ -0,0 +1,22 @@
+require 'open-uri'
+require 'json'
+require 'pp'
+require 'sinatra'
+
+# latitude =  43.0389
+# longitude = -87.9064
+# sensor = false
+# keyword = "food"
+# radius = 50000
+# key = "AIzaSyBH11S2JGj5NBPghmgdid4Z7PXHM8RTFhI"
+# api_call = "https://maps.googleapis.com/maps/api/place/nearbysearch/json\?&location\=#{latitude},#{longitude}&radius\=#{radius}&sensor\=#{sensor}&key\=#{key}&keyword=#{keyword}"
+# jdoc = JSON.parse(open(api_call).read)
+
+jdoc = JSON.parse(File.open('json.txt', 'r').read)
+
+x = jdoc.fetch("results")
+
+get '/' do
+	@results = x
+	erb :home
+end
